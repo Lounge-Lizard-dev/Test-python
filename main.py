bdeae914469241040123fb9c4e02a2569fb21a1d
Lounge Lizard dev <86478645+Lounge-Lizard-dev@users.noreply.github.com>
2021-10-17 21:19:03 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import time
+
+print("Ai: What's your name?")
+name = input("You: ")
+print(" ")
+
+print("Ai: Where are you from?")
+country = input("You: ")
+print(" ")
+
+print("Ai: What is your hobby?")
+hobby = input("You: ")
+print(" ")
+
+print("..")
+time.sleep(2)
+print("...")
+time.sleep(3)
+print("....")
+time.sleep(3)
+
+print(f"Your name: {name}")
+print(f"Count you live in: {country}")
+print(f"Your hobby: {hobby}")
